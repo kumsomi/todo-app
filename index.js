b373e72e9933f3de2ed191c285d032f62db84ce2
Kum Somi <kumsomi.5@gmail.com>
2023-06-03 23:56:42 +0530

    add: added todo features

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const express=require('express');
 const app=express()
+
+const bodyParser=require("body-parser")
 const portNo=3000
 
-app.get('/',(req,res)=>res.send('todo app setup'))
+const todoData=require("./data/todoData.json")
+
+const fs=require('fs')
+
+const {engine} = require("express-handlebars")
+app.engine("handlebars",engine());
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.json())
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+
+app.use(express.urlencoded({ extended: false })) //This middleware is responsible for parsing the form data and making it accessible in req.body.
+
+app.post("/",(req,res)=>{
+    console.log('request body',req.body)
+    const {todoTitle, description}=req.body
+    if(!todoTitle){
+        return res.status(400).send("missing required fields")
+    }
+    else{
+        const newTodo={todoTitle, description, id:todoData.length+1}
+        todoData.push(newTodo)
+        // write the new todo array to the file
+        fs.writeFileSync('./data/todoData.json',JSON.stringify(todoData))
+        // res.json(newTodo);
+        res.redirect("/");
+    }
+    
+})
+app.get('/',(req,res)=>{
+    res.render("home",{todoActivities:todoData})
+})
+// app.patch() //-> for updating
+app.delete('/:id',(req,res)=>{
+    const todoId=req.params.id
+    console.log(todoId)
+}) //-> for deleting
+
+// Todo app-> add new task, show added tasks, update tasks, delete tasks 
+
+
 .listen(portNo,()=>console.log(`server listening at http://localhost:${portNo}`))
